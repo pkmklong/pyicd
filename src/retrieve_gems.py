a92dec49e03947d23d0ccb0ae655943ae7103c11
Patrick Long <patrick.long@gmail.com>
2020-01-01 14:11:54 -0500

    update retrieve_gems.py to collect icd9 desc and perform unzip

---
+++ src/retrieve_gems.py
@@ -0,0 +1,48 @@
+"""
+This script retrieves the raw ICD9 and ICD10 GEMs 
+zip files from www.cms.gov and unzips them in a
+target directory.
+"""
+
+import pandas as pd
+import urllib.request
+import zipfile
+import os
+
+# file access vars
+SAVE_PATH = "../data/raw_data/gems/"
+
+URL_CMS = "https://www.cms.gov/Medicare/Coding/"
+
+URL_GEMS = os.path.join(URL_CMS, "ICD10/Downloads/2018-ICD-10-CM-General-Equivalence-Mappings.zip")
+URL_ICD10  = os.path.join(URL_CMS, "ICD10/Downloads/2018-ICD-10-Code-Descriptions.zip")
+URL_ICD9 =  os.path.join(URL_CMS, "ICD9ProviderDiagnosticCodes/Downloads/ICD-9-CM-v32-master-descriptions.zip")
+
+
+def retrieve_gems_info(url_path: str, file_name: str):
+ 
+    urllib.request.urlretrieve(url_path, file_name)
+
+
+def unzip_dir(file_path: str):
+
+    for f in os.listdir("."):
+       if f.endswith(".zip"):
+            try:
+                zip = zipfile.ZipFile(f)
+                zip.extractall(path=".")
+            except OSError as e:
+                print(f"file: {f}, error: {e}")
+
+
+if __name__=="__main__":
+
+    os.chdir(SAVE_PATH)
+
+    retrieve_gems_info(URL_GEMS, "icd10_gems.zip")
+
+    retrieve_gems_info(URL_ICD10, "icd10_descriptions.zip")
+
+    retrieve_gems_info(URL_ICD9, "icd9_descriptions.zip")
+
+    unzip_dir(SAVE_PATH)
