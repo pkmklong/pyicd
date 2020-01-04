b37aa022775e4572826127b0b7f6ba2d3e8679ac
Patrick Long <patrick.long@gmail.com>
2020-01-04 17:16:41 -0500

    remove commented out variables from process_data.py

--- src/process_data.py
+++ src/process_data.py
@@ -8,14 +8,6 @@ from data_utils.format_data import format_icd, format_desc
 from data_utils.combine_data import add_icd_desc
 from path_utils.path_variables import PathVariables
 
-# file access vars
-#SAVE_PATH = "../data/raw_data/gems/"
-
-#URL_CMS = "https://www.cms.gov/Medicare/Coding/"
-
-#URL_GEMS = os.path.join(URL_CMS, "ICD10/Downloads/2018-ICD-10-CM-General-Equivalence-Mappings.zip")
-#URL_ICD10  = os.path.join(URL_CMS, "ICD10/Downloads/2018-ICD-10-Code-Descriptions.zip")
-#URL_ICD9 =  os.path.join(URL_CMS, "ICD9ProviderDiagnosticCodes/Downloads/ICD-9-CM-v32-master-descriptions.zip")
 
 if __name__=="__main__":
     
