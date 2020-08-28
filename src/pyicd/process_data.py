6fc967e2046978277076f5550cef62adfb411ad1
Patrick Long <patrick.long@gmail.com>
2020-08-28 12:40:39 -0400

    lint with black

--- src/pyicd/process_data.py
+++ src/pyicd/process_data.py
@@ -20,16 +20,15 @@ def main():
     retrieve_gems_info(PathVariables.URL_ICD9, "icd9_descriptions.zip")
     unzip_dir()
 
-    df_icd10= format_icd("2018_I10gem.txt")
-    df_icd9 =  format_icd("2018_I9gem.txt")
+    df_icd10 = format_icd("2018_I10gem.txt")
+    df_icd9 = format_icd("2018_I9gem.txt")
 
-    df_icd9_desc = format_desc("CMS32_DESC_LONG_DX.txt", "icd9", separater = ",")
-    df_icd10_desc = format_desc("icd10cm_codes_2018.txt", "icd10", separater = "//s")
+    df_icd9_desc = format_desc("CMS32_DESC_LONG_DX.txt", "icd9", separater=",")
+    df_icd10_desc = format_desc("icd10cm_codes_2018.txt", "icd10", separater="//s")
 
-    add_icd_desc(df_icd9, df_icd9_desc,
-                df_icd10, df_icd10_desc)
+    add_icd_desc(df_icd9, df_icd9_desc, df_icd10, df_icd10_desc)
 
 
-#if __name__=="__main__":
+# if __name__=="__main__":
 main()
-#collect_gems_data()
+# collect_gems_data()
--- src/pyicd/runner.py
+++ src/pyicd/runner.py
@@ -1,18 +1,21 @@
 import argparse
 from process_data import collect_gems_data
 
+
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("retrieve_gems_data",
-	help = "Pull 2018 ICD9 and ICD10 GEMs mapping data from cms.gov",
-	type = bool)
-    
+    parser.add_argument(
+        "retrieve_gems_data",
+        help="Pull 2018 ICD9 and ICD10 GEMs mapping data from cms.gov",
+        type=bool,
+    )
+
     # Collect args
     args = parser.parse_args()
 
-    #if args.retrieve_gems_data == True:
+    # if args.retrieve_gems_data == True:
     collect_gems_data()
 
 
-if __name__=='__main__':
+if __name__ == "__main__":
     main()
