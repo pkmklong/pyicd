6f345a0c5ebc56a95a730fa45aef95748f3d343e
Patrick <21268383+pkmklong@users.noreply.github.com>
2020-09-05 01:00:16 -0400

    Update test_process_data.py

--- tests/data_utils/test_process_data.py
+++ tests/data_utils/test_process_data.py
@@ -1,7 +1,7 @@
 import pytest
 import pandas as pd
 
-from pyicd.data_utils.format_data import split_flags
+from pyicd.data_utils import format_data as form
 
 @pytest.mark.parametrize("test_input, expected",
 [(1, "0,0,0,0,1"), 
@@ -9,7 +9,19 @@ from pyicd.data_utils.format_data import split_flags
 (101, "0,0,1,0,1")
 ])
 def test_split_flags(test_input, expected):
-    assert split_flags(test_input) == expected
+    assert form.split_flags(test_input) == expected
+
+    
+def test_format_flags():
+    df_input = pd.DataFrame({"flags":["10101"]})
+    df_expected = pd.DataFrame({"approximate":[1],
+                                "no map":[0], 
+                                "combination":[1],
+                                "scenario":[0],
+                                "choice list":[1]})
+    df_result = form.format_flags(df)
+    pd.testing.assert_frame_equal(df_result, df_expected)
+
 
 @pytest.mark.skip()
 def test_format_icd(filename: str):
