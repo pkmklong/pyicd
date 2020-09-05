3da7dfbe9bea7c0c079d0d7033bf088210b8b9b6
Patrick <21268383+pkmklong@users.noreply.github.com>
2020-09-05 01:11:54 -0400

    Update test_icd_converter.py

--- tests/gems_utils/test_icd_converter.py
+++ tests/gems_utils/test_icd_converter.py
@@ -2,10 +2,27 @@
 This run  pytest for  gems converter utils.
 """
 import pytest
+from pyicd.utils.icd_tools import icd9_to_icd10
 
-@pytest.mark.skip(reason="not yet implemented.")
+
+#@pytest.mark.skip(reason="not yet implemented.")
 def test_icd9_to_icd10(icd_code: str):
-    pass
+    
+    df_result = icd9_to_icd10(icd_code = "59972", flag = "approximate")
+    df_expected = pd.DataFrame(
+        {
+            "source":["59972", "59972", "59972"],
+            "icd10":["R311", "R3121", "R3129"],
+            description":[
+                     "BENIGN ESSENTIAL MICROSCOPIC HEMATURIA",
+                     "ASYMPTOMATIC MICROSCOPIC HEMATURIA",
+                     "OTHER MICROSCOPIC HEMATURIA"
+                    ]
+        }
+    )
+    pd.testing.assert_frame_equal(df_result, df_expected)
+       
+
 @pytest.mark.skip(reason="not yet implemented.")
 def test_to_icd9(icd_code: str):
     pass
